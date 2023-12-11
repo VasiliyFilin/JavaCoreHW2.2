c905c57841cd2b986b5e2bdbf98c457fe1566b63
Vasily Filin <pro.vasiliy.filin@gmail.com>
2023-12-11 15:41:57 +0300

    JavaCoreHW2.2 done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        Transport car = new Car("car1", 4);
+        Transport car2 = new Car("car2", 4);
+
+        Transport truck = new Truck("truck1", 6);
+        Transport truck2 = new Truck("truck2", 8);
+
+        Transport bicycle = new Bicycle("bicycle1", 2);
+        Transport bicycle2 = new Bicycle("bicycle2", 2);
+
+        ServiceTransport station = new ServiceTransport();
+        station.check((Car) car);
+        station.check((Car) car2);
+        station.check((Bicycle) bicycle);
+        station.check((Bicycle) bicycle2);
+        station.check((Truck) truck);
+        station.check((Truck) truck2);
+
     }
 }
